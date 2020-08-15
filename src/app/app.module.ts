8520880e4598d6977a124927df94ffd5ff125271
saeed sobati <saeed.sobati@neocard.fi>
2020-08-15 15:39:40 +0300

    test store connected to the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgReduxModule, NgRedux } from '@angular-redux/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RootState, store} from './store';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<RootState>) {
+    ngRedux.provideStore(store);
+  }
+}
