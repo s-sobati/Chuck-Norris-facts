420762b0545fa6266224fcb3efdeb6f7907d94f8
saeed sobati <saeed.sobati@neocard.fi>
2020-08-16 14:34:03 +0300

    apiUrl, apiKey and apiHost added to the dev and prod environment variables.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
+  apiKey: '53a61af534mshea943f753441ac9p1a9f6ejsnd6468530352c',
+  apiHost: 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
+  apiKey: '53a61af534mshea943f753441ac9p1a9f6ejsnd6468530352c',
+  apiHost: 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
 };
 
 /*
