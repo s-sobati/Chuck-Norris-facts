b867cea54c26634f979f34b3c8d1b3aa12b0a08f
saeed sobati <saeed.sobati@neocard.fi>
2020-08-18 19:34:20 +0300

    NoResultFound component added.

---
+++ src/app/Components/Reusables/NoResultFound/index.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-no-result',
+  templateUrl: './index.html',
+  styleUrls: ['./index.scss']
+})
+export class NoResultComponent {}
