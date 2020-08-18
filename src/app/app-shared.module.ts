0917ab294a855f53a4a36b0954e6116a32c8544c
saeed sobati <saeed.sobati@neocard.fi>
2020-08-18 19:35:00 +0300

    components and material modules linked to the app.

--- src/app/app-shared.module.ts
+++ src/app/app-shared.module.ts
@@ -4,23 +4,42 @@ import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material/card';
 import {CommonModule} from '@angular/common';
+import {FactCardComponent} from './Components/Reusables/FactCard';
+import {HeaderComponent} from './Components/Reusables/Header';
+import {MatIconModule} from '@angular/material/icon';
+import {FavoriteButtonComponent} from './Components/Reusables/FavoriteButton';
+import {NoResultComponent} from './Components/Reusables/NoResultFound';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FactCardComponent,
+    FavoriteButtonComponent,
+    NoResultComponent,
+  ],
   imports: [
     CommonModule,
     MatGridListModule,
     MatButtonModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatCardModule,
+    MatIconModule,
   ],
   exports: [
     MatGridListModule,
     MatButtonModule,
     MatListModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatIconModule,
+    HeaderComponent,
+    FactCardComponent,
+    FavoriteButtonComponent,
+    NoResultComponent,
   ],
   providers: []
 })
